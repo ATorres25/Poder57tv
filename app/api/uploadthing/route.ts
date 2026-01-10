2fa62b07adfebec6c126a5c9303353a8330790a8
ATorres25 <alberto.helpdesk@gmail.com>
2026-01-09 19:29:29 -0600

    Fix ESLint version for Next 16 compatibility

--- app/api/uploadthing/route.ts
+++ app/api/uploadthing/route.ts
@@ -2,9 +2,9 @@
 import { createRouteHandler } from "uploadthing/next";
 import { ourFileRouter } from "./core";
 
+// ⬇️ ESTO ES CLAVE
+export const runtime = "nodejs";
+
 export const { GET, POST } = createRouteHandler({
   router: ourFileRouter,
-  config: {
-    token: process.env.UPLOADTHING_TOKEN,
-  },
 });
